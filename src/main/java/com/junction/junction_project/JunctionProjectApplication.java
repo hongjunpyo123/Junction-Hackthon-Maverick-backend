bd211787e19ab015376f220a56d2a4fcd5ab4ef0
june <0721hjp@naver.com>
2025-08-19 23:13:18 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/junction/junction_project/JunctionProjectApplication.java
@@ -0,0 +1,13 @@
+package com.junction.junction_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunctionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunctionProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junction/junction_project/JunctionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junction.junction_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunctionProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
