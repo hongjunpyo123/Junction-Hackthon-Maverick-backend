adbbfa5bc5ae0e99716a96eb5b517bd383ec39a8
june <0721hjp@naver.com>
2025-08-21 22:18:01 +0900

    chore: cors 로컬개발환경, 서버 환경 허용

--- src/main/java/com/junction/junction_project/global/config/WebConfig.java
+++ src/main/java/com/junction/junction_project/global/config/WebConfig.java
@@ -9,9 +9,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:*"
-                )
+            .allowedOrigins(
+                "http://localhost:8080",
+                "http://localhost:3000",
+                "http://localhost:5173",
+                "https://api.junction-maverick.store",
+                "https://www.junction-maverick.store",
+                "https://junction-maverick.store"
+            )
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
